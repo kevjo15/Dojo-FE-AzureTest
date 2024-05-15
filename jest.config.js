2d916a6c376493bc2f177ce7bd0f1edd8ac5107f
HubTicaBB <tijana.ilic87@protonmail.com>
2024-05-15 08:54:39 +0200

    Udpate jest config

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,8 @@ export default {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
   setupFilesAfterEnv: ["./setupTests.ts"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!@testing-library)"],
 };
