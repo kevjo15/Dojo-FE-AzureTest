372acb0e57655e36462884e78a08811c0c54dbce
elliot dahlin <elliot.99@hotmail.com>
2024-06-03 15:12:57 +0200

    Small fix with errors

--- src/components/common/TextBox/TextBox.tsx
+++ src/components/common/TextBox/TextBox.tsx
@@ -5,14 +5,26 @@ interface TextBoxProps {
   type?: string;
   label: string;
   className?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const TextBox: React.FC<TextBoxProps> = ({
   type = "text",
   label,
   className,
+  value,
+  onChange,
 }) => {
-  return <TEInput type={type} label={label} className={className} />;
+  return (
+    <TEInput
+      type={type}
+      label={label}
+      className={className}
+      value={value}
+      onChange={onChange}
+    />
+  );
 };
 
 export default TextBox;
