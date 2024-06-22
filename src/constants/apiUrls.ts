9314f2a5f659f0872019256efa31a71875a876aa
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2024-06-22 17:50:30 +0200

    Update apiUrls.ts

--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -1,3 +1,3 @@
-export const USERS_API_URL = "https://localhost:7036/api/User/GetAllUsers";
+export const USERS_API_URL = "https://din-backend-url.azurewebsites.net/api/User/GetAllUsers";
 
-export const REGISTER_API_URL = "https://localhost:7036/api/User/register";
+export const REGISTER_API_URL = "https://din-backend-url.azurewebsites.net/api/User/register";
